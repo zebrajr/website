8c7ada95952e33f9477eed9c693718645a70aa0b
Carlos Sousa <me@carlossousa.tech>
2025-06-16 18:11:48 +0200

    add lily58 pro to projects

--- frontend/src/components/ProjectsText.js
+++ frontend/src/components/ProjectsText.js
@@ -48,47 +48,59 @@ const statuspage = {
   sourceURL: 'https://github.com/TwiN/gatus',
 };
 
+const lily58pro = {
+  text: 'Lily58 Pro Keyboard',
+  url: 'https://wiki.carlossousa.tech/doku.php?id=engineering:computer_science:projects:lily58_pro_diy_custom_keyboard',
+  sourceName: 'QMK and ZMK Firmware',
+  sourceURL: 'https://github.com/zebrajr/lily58_keyboard',
+};
+
 function ProjectsText() {
-  return (
-    <div style={mainDivStyle}>
-      <section id={sectionID} style={mainSectionStyle}>
+    return (
+        <div style={mainDivStyle}>
+        <section id={sectionID} style={mainSectionStyle}>
         <Typography variant="h4" style={typographyStyles.largeBoldHeaderPrimary}>
-          {titleText}
+        {titleText}
         </Typography>
         <Typography variant="h9" style={typographyStyles.subtleSubheading}>
-          {subTitleText}
+        {subTitleText}
         </Typography>
         <Typography variant="body1" paragraph style={typographyStyles.colorfulEmphasis}>
-          Let's start by the quickest, simplest (and one of the most useful for multiple reasons) - A <MuiLink href={statuspage.url} target="_blank">{statuspage.text}</MuiLink>. A <MuiLink href={statuspage.sourceURL} target="_blank">{statuspage.sourceName}</MuiLink> instance so I know if everything is running at a quick glance and to inform others (and myself) of all the services that I host and run, or at least the ones that are meant to be easy to find.
+        Let's begin with one of the most useful projects - a <MuiLink href={statuspage.url} target="_blank">{statuspage.text}</MuiLink>. This <MuiLink href={statuspage.sourceURL} target="_blank">{statuspage.sourceName}</MuiLink> allows me to quickly check the status of my hosted services and provide transparency to others.
         </Typography>
         <Typography variant="body1" paragraph style={typographyStyles.colorfulEmphasis}>
-          So, one of the many things I enjoy having (mostly for me, to be fair) is my <MuiLink href={wikipediaData.url} target="_blank">{wikipediaData.text}</MuiLink>. Nothing fancy, just hosting <MuiLink href={wikipediaData.sourceURL} target="_blank">{wikipediaData.sourceName}</MuiLink> so I can keep some notes, references (maybe some guides?), and just random thoughts.
+        As a hardware and software enthusiast, I've built two <MuiLink href={lily58pro.url} target="_blank">{lily58pro.text}</MuiLink> - a wired version for home use and a wireless one for work. The <MuiLink href={lily58pro.sourceURL} target="_blank">{lily58pro.sourceName}</MuiLink> is available for those interested in creating their own Lily 58 Pro Keyboard variant.
+        </Typography>
+        <Typography variant="body1" paragraph style={typographyStyles.colorfulEmphasis}>
+        I maintain a personal <MuiLink href={wikipediaData.url} target="_blank">{wikipediaData.text}</MuiLink>, primarily for my own use. This <MuiLink href={wikipediaData.sourceURL} target="_blank">{wikipediaData.sourceName}</MuiLink> serves as a repository for notes, references, guides, and random thoughts.
         </Typography>
         <Typography variant="body1" paragraph>
-          I also host this <MuiLink href={traveltracer.url} target="_blank">{traveltracer.text}</MuiLink>, source code in <MuiLink href={traveltracer.sourceURL} target="_blank">{traveltracer.sourceName}</MuiLink>. A one-page application that shows Markers on a rendered GeoJSON file with Leaflet, to display a few of the places I either visited or want to visit.
+        I've also developed a <MuiLink href={traveltracer.url} target="_blank">{traveltracer.text}</MuiLink> - a single-page application using Leaflet to display markers on a GeoJSON file, showcasing places I've visited or wish to explore. The source code is available on <MuiLink href={traveltracer.sourceURL} target="_blank">{traveltracer.sourceName}</MuiLink>.
         </Typography>
         <Typography variant="body1" paragraph>
-          I also run an <MuiLink href={archiveData.url} target="_blank">{archiveData.text}</MuiLink> where I keep articles that I either reference or enjoy re-reading, and don't want them to be lost due to the natural state of the internet. In case you want to have your own, it's called <MuiLink href={archiveData.sourceURL} target="_blank">{archiveData.sourceName}</MuiLink>. I have my issues with it, don't get me started on the topic.
+        My <MuiLink href={archiveData.url} target="_blank">{archiveData.text}</MuiLink> preserves articles I want to reference or revisit, preventing them from being lost to the transient nature of the internet. Powered by <MuiLink href={archiveData.sourceURL} target="_blank">{archiveData.sourceName}</MuiLink> (though I have my reservations about it).
         </Typography>
         <Typography variant="body1" paragraph>
-          I'm also hosting a calculator, <MuiLink href={reactoCalcData.url} target="_blank">{reactoCalcData.text}</MuiLink>. Source code also available in <MuiLink href={reactoCalcData.sourceURL} target="_blank">{reactoCalcData.sourceName}</MuiLink>. It's a simple calculator that is useful for specific situations, mainly financial ones. Nothing fancy, just something that was put together (semi-)quickly.
+        I host a specialized <MuiLink href={reactoCalcData.url} target="_blank">{reactoCalcData.text}</MuiLink>, a calculator designed for specific financial scenarios. The source code is available on <MuiLink href={reactoCalcData.sourceURL} target="_blank">{reactoCalcData.sourceName}</MuiLink>.
         </Typography>
         <Typography variant="body1" paragraph>
-          I'm still hosting a <MuiLink href={tornIndexerData.url} target="_blank">{tornIndexerData.text}</MuiLink> for <MuiLink href={tornIndexerData.sourceURL} target="_blank">{tornIndexerData.sourceName}</MuiLink>. I honestly stopped using it, like, 1 month after I finished the Proof of Concept (PoC), but some players still use it, so, guess it stays up. If for whatever reason you are interested in it ( don't code shame me :( ), you will find the Source Code on my GitHub.
+        I'm still maintaining a <MuiLink href={tornIndexerData.url} target="_blank">{tornIndexerData.text}</MuiLink> for <MuiLink href={tornIndexerData.sourceURL} target="_blank">{tornIndexerData.sourceName}</MuiLink>. Though I stopped using it shortly after the Proof of Concept (PoC), some players continue to find it useful.
         </Typography>
         <Typography variant="body1" paragraph>
-          I also host a few more services, but either I don't want or can't share publicly.
+        Several additional services remain undisclosed due to privacy or confidentiality reasons.
         </Typography>
         <Typography variant="body1" paragraph>
-          Among other services that I either prefer not to disclose or cannot share publicly, I also manage a HomeLab.
-          <br />It includes a Proxmox setup with VMs and containers, 3-2-1 backups, and more, mostly for the purpose of experimenting with "Linux ISOs," learning, and testing.
+        My HomeLab is a comprehensive setup that includes:
+        <br />• Proxmox with VMs and containers
+        <br />• 3-2-1 backup strategy
+        <br />Primarily used for experimentation, learning, and testing.
         </Typography>
         <Typography variant="body1" paragraph>
-          These projects represent a blend of both personal enjoyment and practical utility in my tech pursuits.
+        These projects reflect my passion for technology, balancing personal curiosity with practical innovation.
         </Typography>
-      </section>
-    </div>
-  );
+        </section>
+        </div>
+    );
 }
 
 export default ProjectsText;
